b12898f0a76933b02aa0bfebecec26e4963467ba
sthakuri <suvashshah.thakuri@gmail.com>
2016-02-05 11:52:25 -0800

    Send message form implementation

--- Photos.ePaila.com/Controllers/HomeController.cs
+++ Photos.ePaila.com/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Photos.ePaila.com.Models;
 using Photos.ePaila.com.Models.Channel;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Photos.ePaila.com.Controllers
 {
@@ -20,5 +23,28 @@ namespace Photos.ePaila.com.Controllers
             return View(model);
         }
 
+        public async Task SendEmail(string name, string email, string message)
+        {
+            //var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+            //var msg = new MailMessage();
+            //msg.To.Add(new MailAddress(""));  // replace with valid value 
+            //msg.From = new MailAddress(email);  // replace with valid value
+            //msg.Subject = "New Message from ePaila";
+            //msg.Body = string.Format(body, name, email, message);
+            //msg.IsBodyHtml = true;
+            //using (var smtp = new SmtpClient())
+            //{
+            //    var credential = new NetworkCredential
+            //        {
+            //            UserName = "", // replace with valid value
+            //            Password = "" // replace with valid value
+            //        };
+            //    smtp.Credentials = credential;
+            //    smtp.Host = "smtp.gmail.com";
+            //    smtp.Port = 587;
+            //    smtp.EnableSsl = true;
+            //    await smtp.SendMailAsync(msg);
+            //}
+        }
     }
 }
